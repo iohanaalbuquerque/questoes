5979182b0bc5191cf17d1c775f449913cca882f1
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-27 21:01:39 -0300

    fundamentos_questao01.js

--- fundamentos_questao01.js
+++ fundamentos_questao01.js
@@ -1,1 +1,17 @@
+## 1) Escreva um programa em Javascript que exiba propriedades de data e hora no seguinte
+formato: ◦ Hoje é: [dia da semana].◦ A hora atual é: [hora atual no formato de HH:MM:SS].
+
+function exibirDiaDaSemana() {
+    const diasDaSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const data = new Date();
+    return diasDaSemana[data.getDay()];
+}
+
+function exibirHoraAtual() {
+    const atual= new Date();
+    const horas = String(atual.getHours()).padStart(2, '0'); 
+    const minutos = String(atual.getMinutes()).padStart(2, '0'); 
+    const segundos = String(atual.getSeconds()).padStart(2, '0'); 
+    return `${horas}:${minutos}:${segundos}`;
+}
 
