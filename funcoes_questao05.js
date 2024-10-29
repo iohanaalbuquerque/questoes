ada103d1b2f768cd141c27e6dce957dba66461d4
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 22:05:34 -0300

    funcoes_questao05.js

--- funcoes_questao05.js
+++ funcoes_questao05.js
@@ -1,1 +1,15 @@
+function encontrarMaiorPalavra(frase) {
+    const palavras = frase.split(" ");
+    
+    let maiorPalavra = "";
+    
+    for (let palavra of palavras) {
+        if (palavra.length > maiorPalavra.length) {
+            maiorPalavra = palavra;
+        }
+    }
+    
+    return maiorPalavra;
+}
+
 
