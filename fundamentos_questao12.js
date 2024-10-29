8442ca995e73be8657e78808b6c1365d0ddf66ca
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:53:13 -0300

    fundamentos_questao12.js

--- fundamentos_questao12.js
+++ fundamentos_questao12.js
@@ -1,1 +1,10 @@
+function unirElementos(lista) {
+    return lista.join("#");
+}
+
+13) Escreva um programa que leia uma lista de números. Eleve cada um dos números a 3 e
+depois some o total de valores obtidos.
+function somaCubos(lista) {
+    return lista.reduce((soma, numero) => soma + Math.pow(numero, 3), 0);
+}
 
