37e126e758a543de32c2c191acde32b3f0972dd4
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:40:09 -0300

    fundamentos_questao06.js

--- fundamentos_questao06.js
+++ fundamentos_questao06.js
@@ -1,1 +1,16 @@
+function encontraNumeroFrequente(array) {
+    const frequencia = {};
+    let numeroFrequente = array[0];
+    let maiorOcorrencia = 0;
+    for (let num of array) {
+        frequencia[num] = (frequencia[num] || 0) + 1;
+
+           if (frequencia[num] > maiorOcorrencia) {
+            maiorOcorrencia = frequencia[num];
+            numeroFrequente = num;
+        }
+    }
+
+    return numeroFrequente;
+}
 
