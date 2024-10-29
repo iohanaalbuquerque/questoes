c5a5340dd22aaad3cda0c2cadda70d6dfeb050c9
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:45:30 -0300

    fundamentos_questao09.js

--- fundamentos_questao09.js
+++ fundamentos_questao09.js
@@ -1,1 +1,7 @@
+function somaTamanhosPalavras(frase) {
+    const palavras = frase.split(" ");
 
+    const somaTamanhos = palavras.reduce((soma, palavra) => soma + palavra.length, 0);
+
+    return somaTamanhos;
+}
