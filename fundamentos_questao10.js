4e7610927621657c69d11c202f165e11a5ce7a3f
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:45:50 -0300

    fundamentos_questao10.js

--- fundamentos_questao10.js
+++ fundamentos_questao10.js
@@ -1,1 +1,11 @@
+function converterNome(nomeCompleto) {
+    const partes = nomeCompleto.split(" ");
+
+    const primeiroNome = partes.shift(); 
+    const sobrenomes = partes.reverse();
+
+    const resultado = sobrenomes.map(sobrenome => sobrenome[0].toUpperCase() + ".").join(" ") + " " + primeiroNome;
+
+    return resultado;
+}
 
