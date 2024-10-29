8bd8d0865a2a6808189799a80a389938897cdbcb
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:41:52 -0300

    fundamentos_questao08.js

--- fundamentos_questao08.js
+++ fundamentos_questao08.js
@@ -3,12 +3,6 @@ function inverterFrase(frase) {
 }
 
 
-9) Escreva um programa que calcule a soma dos tamanhos de todas as palavras em uma frase.
-function somaTamanhosPalavras(frase) {
-    const palavras = frase.split(" ");
 
-    const somaTamanhos = palavras.reduce((soma, palavra) => soma + palavra.length, 0);
 
-    return somaTamanhos;
-}
 
