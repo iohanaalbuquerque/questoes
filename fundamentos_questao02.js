87928a272ad4a8a19000a7f6750e0cb4ab6fb5b3
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:35:09 -0300

    fundamentos_questao02.js

--- fundamentos_questao02.js
+++ fundamentos_questao02.js
@@ -1,5 +1,3 @@
-# 2) Escreva um programa em Javascript que calcule a área de um triângulo. Você pode fornecer os valores de cada lado em variáveis separadas ou em uma lista de valores.
-
 function calcularAreaTriangulo(lados) {
     if (lados.length !== 3) {
         return "Informe os três valores dos lados do triângulo.";
