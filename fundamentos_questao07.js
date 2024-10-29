38d5417120fa90930a5592e8cb07e997ffad00d2
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:40:35 -0300

    fundamentos_questao07.js

--- fundamentos_questao07.js
+++ fundamentos_questao07.js
@@ -1,1 +1,14 @@
+function modificarFrase(frase) {
+     const palavras = frase.split(" ");
+
+       const palavrasModificadas = palavras.map(palavra => {
+        if (palavra.length < 5) {
+            return palavra.toUpperCase();
+        } else {
+            return palavra.toLowerCase();
+        }
+    });
+
+    return palavrasModificadas.join(" ");
+}
 
