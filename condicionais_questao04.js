a8fafb81afe4e5adbfefb029a1042e3b949349ae
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:59:20 -0300

    condicionais_questao04.js

--- condicionais_questao04.js
+++ condicionais_questao04.js
@@ -1,1 +1,15 @@
+function calcularMediaAlunos(alunos) {
+    for (let i = 0; i < alunos.length; i++) {
+        const nome = alunos[i][0];
+        const notas = alunos[i][1];
+
+        
+        const somaNotas = notas.reduce((soma, nota) => soma + nota, 0);
+        const media = somaNotas / notas.length;
+
+        const status = media >= 70 ? "Aprovado" : "Reprovado";
+
+        console.log(`${nome}, média ${media.toFixed(2)}, ${status}`);
+    }
+}
 
