84a22e90799ffa1dccb4eb6e31ca86e5f393486c
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 22:00:23 -0300

    funcoes_questao03.js

--- funcoes_questao03.js
+++ funcoes_questao03.js
@@ -1,1 +1,11 @@
+function encontrarMaiorMenor(lista) {
+valores
+    const maior = Math.max(...lista);
+    const menor = Math.min(...lista);
+
+    return {
+        maior: maior,
+        menor: menor
+    };
+}
 
