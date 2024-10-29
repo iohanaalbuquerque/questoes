2c5d19b64297e4b5d4ff9e8de4ea869a371f6c8d
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:50:39 -0300

    fundamentos_questao11.js

--- fundamentos_questao11.js
+++ fundamentos_questao11.js
@@ -1,1 +1,8 @@
+function ultimosElementos(lista, n) 
+    if (n > lista.length) {
+        return "O n deve ser menor que a quantidade de elementos da lista.";
+    }
+    
+      return lista.slice(-n);
+}
 
