71f69c457607af4de4fa9fa19d9c499235d00263
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 22:00:04 -0300

    funcoes_questao02.js

--- funcoes_questao02.js
+++ funcoes_questao02.js
@@ -1,1 +1,19 @@
+function contarVogaisConsoantes(frase) {
+    const vogais = "aeiouAEIOU"; // Define as vogais
+    let contagemVogais = 0;
+    let contagemConsoantes = 0;
+
+    for (let char of frase) {
+        if (vogais.includes(char)) {
+            contagemVogais++;
+        } else if (char.match(/[a-zA-Z]/)) {
+            contagemConsoantes++;
+        }
+    }
+
+    return {
+        vogais: contagemVogais,
+        consoantes: contagemConsoantes
+    };
+}
 
