f7f885dc92db59bf4d171f404c1f8695c9bfcecc
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:38:40 -0300

    fundamentos_questao05.js

--- fundamentos_questao05.js
+++ fundamentos_questao05.js
@@ -1,1 +1,12 @@
+function checarNumeros(num1, num2, num3) {
+    if (
+        (num1 >= 50 && num1 <= 99) &&
+        (num2 >= 50 && num2 <= 99) &&
+        (num3 >= 50 && num3 <= 99)
+    ) {
+        console.log("Todos os números estão entre 50 e 99.");
+    } else {
+        console.log("Algum ou mais dos números não estão entre 50 e 99.");
+    }
+}
 
