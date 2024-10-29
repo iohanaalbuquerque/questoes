9bba6d6073b356a554516409b4ba66de94b6090f
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:56:49 -0300

    condicionais_questao01.js

--- condicionais_questao01.js
+++ condicionais_questao01.js
@@ -1,1 +1,10 @@
+function compararNumeros(num1, num2) {
+    if (num1 > num2) {
+        return `${num1} é maior que ${num2}`;
+    } else if (num2 > num1) {
+        return `${num2} é maior que ${num1}`;
+    } else {
+        return `${num1} e ${num2} são iguais`;
+    }
+}
 
