310fabb0cf985b4c318073e88159499cd2224a45
iohanaalbuquerque <144938635+iohanaalbuquerque@users.noreply.github.com>
2024-10-28 21:37:43 -0300

    fundamentos_questao03.js

--- fundamentos_questao03.js
+++ fundamentos_questao03.js
@@ -1,1 +1,11 @@
+function celsiusParaFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitParaCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+console.log(tempCelsius + "°C em Fahrenheit é: " + celsiusParaFahrenheit(tempCelsius) + "°F");
+console.log(tempFahrenheit + "°F em Celsius é: " + fahrenheitParaCelsius(tempFahrenheit) + "°C");
 
